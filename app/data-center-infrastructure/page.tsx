e398a36fee6edf3f35546a9456b0734f9fe1556a
jassim <jassimtaskm@gmail.com>
2026-02-27 15:31:56 +0530

    Update page.tsx

--- app/data-center-infrastructure/page.tsx
+++ app/data-center-infrastructure/page.tsx
@@ -187,7 +187,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-		</div>
         {/* FOOTER START */}
         <footer
           className="site-footer footer-dark"
